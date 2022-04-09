49b52e040196f4bf68bbfdbb05cf727317bd8e44
Chandu-bs <chandu@bluesapience.com>
2022-04-09 18:37:05 +0530

    Completed integration testing

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,20 +7,21 @@ void main(){
 
 IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-group('Performing bmi app test', (){
+
+
+testWidgets(
+  "test description",
+  (WidgetTester tester) async {
+  //Arrange
+  app.main();
+  await tester.pumpAndSettle();
 
 var _weightDecrementButton =find.byKey(const Key('weight_minus'));
 var _ageDecrementButton =find.byKey(const Key('age_minus'));
 var _weightIncrementButton =find.byKey(const Key('weight_plus'));
 var _ageIncrementButton =find.byKey(const Key('age_plus'));
 var _calculateButton =find.byKey(const Key('Calculate'));
-
-testWidgets(
-  "test description",
-  (WidgetTester tester) async {
-  app.main();
-  await tester.pumpAndSettle();
-  //Arrange
+  //Act
   await tester.tap(_weightIncrementButton);
   await tester.pumpAndSettle();
   await tester.tap(_ageIncrementButton);
@@ -28,10 +29,20 @@ testWidgets(
   await tester.tap(_calculateButton);
   await tester.pumpAndSettle();
 
+  await Future.delayed(const Duration(seconds: 3));
+
+  final _resultValue = '21.87';
+  final _resultText = 'NORMAL';
+  final _resultDescription = 'You are perfect, Good job and stay fit!';
+
+  //Assert
+
+  expect(_resultValue, findsOneWidget);
+  expect(_resultText, findsOneWidget);
+  expect(_resultDescription, findsOneWidget);
+
   },
 );
 
-
-});
 
 }
